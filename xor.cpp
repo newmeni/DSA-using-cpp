34e1296b34f92ba69bcf78b62d58a04d70d2b48a
newmeni <83723137+newmeni@users.noreply.github.com>
2023-06-11 17:49:32 +0530

    Update xor.cpp

--- xor.cpp
+++ xor.cpp
@@ -1,9 +1,9 @@
+\\Xor of consecutive elements in array
 #include <bits/stdc++.h>
 using namespace std;
 int main (){
     int a;
     cin>>a;
-
     int array[a];
     for(int i=0;i<a;i++){
         cin>>array[i];
