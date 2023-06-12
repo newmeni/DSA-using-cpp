7856dce6c42d42dca94f01141c462d9d9e927196
newmeni <83723137+newmeni@users.noreply.github.com>
2023-06-12 12:00:15 +0530

    Update intersection.cpp

--- intersection.cpp
+++ intersection.cpp
@@ -13,5 +13,13 @@ int main(){
     for(int i=0;i<b;i++){
         cin>>a2[i];
     }
-
+    for(int i=0;i<a;i++){
+    for(int j=0;j<b;j++){
+        if(a1[i]==a2[j]){
+            cout<<a1[i]<<" ";
+            a2[j]=-2;
+        }
+    }
+}
+return 0;
 }
